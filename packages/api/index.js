2aa388f7fc519d487e561f9f37a2338d00485063
Luiz Felicio <unifelicio@gmail.com>
2020-09-11 10:17:16 -0300

    Feature/api/testing docker deploy (#3)
    * refactor: change response text again
    
    * refactor: change response text again

--- packages/api/index.js
+++ packages/api/index.js
@@ -4,7 +4,7 @@ const log = require("@project/common");
 
 app.use(cors());
 app.get("/", (req, res) =>
-  res.send({ text: "Hello. This text was changed for Heroku testing" })
+  res.send({ text: "Hello from container on Heroku" })
 );
 
 app.listen(80, () => log(`Listening on 80...`));
