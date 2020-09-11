75b7a47aadb1f2176b82394d3070505843ab5bbb
Luiz Felicio <unifelicio@gmail.com>
2020-09-11 09:53:33 -0300

    refactor: change response text so we can test on heroku (#2)

--- packages/api/index.js
+++ packages/api/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const log = require("@project/common");
 
 app.use(cors());
-app.get("/", (req, res) => res.send({ text: "Hello Word" }));
+app.get("/", (req, res) =>
+  res.send({ text: "Hello. This text was changed for Heroku testing" })
+);
 
 app.listen(3333, () => log(`Listening on 3333...`));
