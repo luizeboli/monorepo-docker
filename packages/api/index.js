d60a9b42a4f450715323dc28ebbc26023fe4c395
luizeboli <unifelicio@gmail.com>
2020-09-11 11:42:33 -0300

    fix: heroku set dynamic ports

--- packages/api/index.js
+++ packages/api/index.js
@@ -6,4 +6,6 @@ app.get("/", (req, res) =>
   res.send({ text: "Hello sent from container on Heroku!!" })
 );
 
-app.listen(3333, () => console.log(`Listening on 3333...`));
+app.listen(process.env.PORT || 80, () =>
+  console.log(`Listening on ${process.env.PORT || 80}...`)
+);
