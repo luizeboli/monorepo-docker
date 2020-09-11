99f54da363c5223ad9369249c88ae3b8d1fd4ecb
Luiz Felicio <unifelicio@gmail.com>
2020-09-11 11:45:07 -0300

    Api staging (#23)
    * refactor: change response text again (#16)
    
    * build: using heroku cli
    
    * Feature/api/test (#18)
    
    * refactor: change response text again
    
    * refactor: change response text again
    
    * refactor: remove common package
    
    * Feature/api/test (#19)
    
    * refactor: change response text again
    
    * refactor: change response text again
    
    * refactor: remove common package
    
    * build: fix dockerfile
    
    * Feature/api/test (#20)
    
    * refactor: change response text again
    
    * refactor: change response text again
    
    * refactor: remove common package
    
    * build: fix dockerfile
    
    * chore: remove common dep
    
    * Feature/api/test (#21)
    
    * refactor: change response text again
    
    * refactor: change response text again
    
    * refactor: remove common package
    
    * build: fix dockerfile
    
    * chore: remove common dep
    
    * build: change port
    
    * Feature/api/test (#22)
    
    * refactor: change response text again
    
    * refactor: change response text again
    
    * refactor: remove common package
    
    * build: fix dockerfile
    
    * chore: remove common dep
    
    * build: change port
    
    * fix: heroku set dynamic ports

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,10 +1,11 @@
 const app = require("express")();
 const cors = require("cors");
-const log = require("@project/common");
 
 app.use(cors());
 app.get("/", (req, res) =>
-  res.send({ text: "Hello sent from container on Heroku" })
+  res.send({ text: "Hello sent from container on Heroku!!" })
 );
 
-app.listen(80, () => log(`Listening on 80...`));
+app.listen(process.env.PORT || 80, () =>
+  console.log(`Listening on ${process.env.PORT || 80}...`)
+);
